50a0835bfb38fc7d86c57c33fda6875c06ed0d96
Jacob Van De Weert <vandeweertj@gmail.com>
2018-11-19 23:31:58 -0500

    add homebrew and zsh setup

--- setup.sh
+++ setup.sh
@@ -2,4 +2,14 @@
 
 root="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
+# homebrew and required packages
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install zsh fortune cowsay
+
+# zsh setup
+sudo echo "$(which zsh)" >> /etc/shells
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# copy over dotfiles
 cp -r "$root/home/".* ~
