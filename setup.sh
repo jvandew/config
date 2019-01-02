00bf4d753306dc11c3004548306fd8abb340f676
Jacob Van De Weert <vandeweertj@gmail.com>
2019-01-01 20:30:08 -0600

    check before adding zsh to /etc/shells

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ root="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 brew install zsh fortune cowsay gpg wget
 
 # zsh setup
-sudo echo "$(which zsh)" >> /etc/shells
+sudo sh -c 'grep -q -F "$(which zsh)" /etc/shells || echo "$(which zsh)" >> /etc/shells'
 chsh -s $(which zsh)
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
