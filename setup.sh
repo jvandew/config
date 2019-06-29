40fcfdf31d082a0ceca2a8c1edbdf3a8e33355b1
Jacob Van De Weert <vandeweertj@gmail.com>
2019-06-29 14:52:49 -0400

    install java and scala

--- setup.sh
+++ setup.sh
@@ -5,7 +5,8 @@ root="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
 # homebrew and required packages
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install zsh fortune cowsay gpg wget
+brew cask install java
+brew install cowsay fortune gpg scala wget zsh
 
 # zsh setup
 sudo sh -c 'grep -q -F "$(which zsh)" /etc/shells || echo "$(which zsh)" >> /etc/shells'
