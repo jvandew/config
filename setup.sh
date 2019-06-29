85114b782a6673a7bdafd374e1e3044d2ea8a215
Jacob Van De Weert <vandeweertj@gmail.com>
2019-06-29 17:28:26 -0400

    install byobu and imagemagick

--- setup.sh
+++ setup.sh
@@ -6,7 +6,15 @@ root="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 # homebrew and required packages
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew cask install java
-brew install cowsay fortune gpg scala wget zsh
+brew install \
+  byobu \
+  cowsay \
+  fortune \
+  gpg \
+  imagemagick \
+  scala \
+  wget \
+  zsh
 
 # zsh setup
 sudo sh -c 'grep -q -F "$(which zsh)" /etc/shells || echo "$(which zsh)" >> /etc/shells'
